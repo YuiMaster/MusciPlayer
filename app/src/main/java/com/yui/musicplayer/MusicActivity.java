10f57ced08cb85529d4248f63382132def98f0e5
廖誉欢 <liaoyuhuan@hudunsoft.com>
2018-04-08 10:57:57 +0800

    细节提交

--- app/src/main/java/com/yui/musicplayer/MusicActivity.java
+++ app/src/main/java/com/yui/musicplayer/MusicActivity.java
@@ -4,17 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
+ * 音乐播放器主页面
+ *
  * @author liaoyuhuan
  * @name ${PROJECT_NAME}
  * @class
  * @time 2018/4/8  10:38
  * @description
  */
-public class MainActivity extends AppCompatActivity {
+public class MusicActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_music);
     }
 }
