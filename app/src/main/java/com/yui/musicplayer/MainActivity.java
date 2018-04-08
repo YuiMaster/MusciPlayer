f46918bef60d7b02be276ef1ca27f98244b72637
廖誉欢 <liaoyuhuan@hudunsoft.com>
2018-04-08 10:39:55 +0800

    代码初始化

---
+++ app/src/main/java/com/yui/musicplayer/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yui.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author liaoyuhuan
+ * @name ${PROJECT_NAME}
+ * @class
+ * @time 2018/4/8  10:38
+ * @description
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
